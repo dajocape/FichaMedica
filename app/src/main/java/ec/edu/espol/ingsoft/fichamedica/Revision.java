1d22bfd0b1b7bf60a2130cb3cdb751243bb940c2
anrosant <anrosant@espol.edu.ec>
2018-01-07 18:00:51 -0600

    layout de examen_fisico

---
+++ app/src/main/java/ec/edu/espol/ingsoft/fichamedica/Revision.java
@@ -0,0 +1,12 @@
+package ec.edu.espol.ingsoft.fichamedica;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class Revision extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.revision);
+    }
+}
---
+++ app/src/main/java/ec/edu/espol/ingsoft/fichamedica/examen_fisico.java
@@ -0,0 +1,16 @@
+package ec.edu.espol.ingsoft.fichamedica;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Anny on 07/01/2018.
+ */
+
+public class examen_fisico extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.examen_fisico);
+    }
+}
