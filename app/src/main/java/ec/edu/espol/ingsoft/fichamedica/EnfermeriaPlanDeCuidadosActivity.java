ba9d1f4cd5e4fab50062204fa04607cce136aae9
jorge18link <18linkjorge@gmail.com>
2018-01-13 12:32:51 -0500

    Creacion de los layouts Preescripcion de consulta y diagnostico y plan de cuidados de enfermeria

---
+++ app/src/main/java/ec/edu/espol/ingsoft/fichamedica/EnfermeriaPlanDeCuidadosActivity.java
@@ -0,0 +1,13 @@
+package ec.edu.espol.ingsoft.fichamedica;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EnfermeriaPlanDeCuidadosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_enfermeria_plan_de_cuidados);
+    }
+}
