0cab9d604c71cd6c1992f014ff98fb1360ba199a
renato <rillesca@espol.edu.ec>
2018-01-19 15:58:25 -0500

    guardado signos vitales

--- app/src/main/java/ec/edu/espol/ingsoft/fichamedica/historia_medica/tabs/consulta_medica/SignosVitalesFragment.java
+++ app/src/main/java/ec/edu/espol/ingsoft/fichamedica/historia_medica/tabs/consulta_medica/SignosVitalesFragment.java
@@ -6,18 +6,40 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.EditText;
+import android.widget.Button;
 import ec.edu.espol.ingsoft.fichamedica.R;
+import ec.edu.espol.ingsoft.fichamedica.model.SignosVitales;
 
 public class SignosVitalesFragment extends Fragment {
 
     private static final String TAG = "SignosVitalesFragment";
+    EditText ET_presionArterial,
+            ET_pulsoPorMinuto,
+            ET_temperatura,
+            ET_motivoConsulta;
+    Button BTN_guardar;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater .inflate(R.layout.signos_vitales_fragment,container,false);
+        View view = inflater.inflate(R.layout.signos_vitales_fragment,container,false);
 
+        ET_presionArterial = view.findViewById(R.id.ET_presionArterial);
+        ET_pulsoPorMinuto = view.findViewById(R.id.ET_pulsoPorMinuto);
+        ET_temperatura = view.findViewById(R.id.ET_temperatura);
+        BTN_guardar = view.findViewById(R.id.BTN_guardar);
+
+        BTN_guardar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SignosVitales signosVitalesPaciente = new SignosVitales(
+                        Integer.parseInt(ET_presionArterial.getText().toString()),
+                        Integer.parseInt(ET_pulsoPorMinuto.getText().toString()),
+                        Integer.parseInt(ET_temperatura.getText().toString()));
+                signosVitalesPaciente.save();
+            }
+        });
         return view;
     }
 }
