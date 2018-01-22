d94d6dff7bba9bb73dedc7c7445bb3949faf58b5
renato <rillesca@espol.edu.ec>
2018-01-21 21:16:59 -0500

    version 0.1.24 guardado de motivo de atencion enfermeria

--- app/src/main/java/ec/edu/espol/ingsoft/fichamedica/historia_medica/tabs/consulta_enfermeria/SignosVitalesEnfermeriaContentFragment.java
+++ app/src/main/java/ec/edu/espol/ingsoft/fichamedica/historia_medica/tabs/consulta_enfermeria/SignosVitalesEnfermeriaContentFragment.java
@@ -8,42 +8,43 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import ec.edu.espol.ingsoft.fichamedica.R;
+import ec.edu.espol.ingsoft.fichamedica.model.SignosVitales;
 
 public class SignosVitalesEnfermeriaContentFragment extends Fragment {
 
     private static final String TAG = "SignosVitalesEnfermeriaContentFragment";
 
-    EditText txtPresionArterial,
-            txtPulsoPorMinuto,
-            txtTemperatura;
-    Button btnGuardar;
+    EditText txt_presion_arterial,
+            txt_pulso_por_minuto,
+            txt_temperatura;
+    Button btn_guardar;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.signos_vitales_enfermeria_content_fragment, container, false);
 
-        txtPresionArterial = view.findViewById(R.id.txt_presion_arterial);
-        txtPulsoPorMinuto = view.findViewById(R.id.txt_pulso_por_minuto);
-        txtTemperatura = view.findViewById(R.id.txt_temperatura);
-        btnGuardar =     view.findViewById(R.id.btn_guardar);
+        txt_presion_arterial = view.findViewById(R.id.txt_presion_arterial);
+        txt_pulso_por_minuto = view.findViewById(R.id.txt_pulso_por_minuto);
+        txt_temperatura = view.findViewById(R.id.txt_temperatura);
+        btn_guardar =     view.findViewById(R.id.btn_guardar);
 
-//        btnGuardar.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                SignosVitales signosVitalesPaciente = new SignosVitales();
-//
-//                signosVitalesPaciente.setPresion_arterial(Integer.parseInt(txtPresionArterial.getText().toString()));
-//                signosVitalesPaciente.setFrecuencia_cardiaca(Integer.parseInt(txtPulsoPorMinuto.getText().toString()));
-//                signosVitalesPaciente.setTemperatura(Integer.parseInt(txtTemperatura.getText().toString()));
-//
-//                signosVitalesPaciente.save();
-//
-//                Toast.makeText(getContext(), "Signos Vitales Guardados exitosamente", Toast.LENGTH_SHORT).show();
-//            }
-//        });
+        btn_guardar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SignosVitales signosVitalesEnfermeria = new SignosVitales();
+                signosVitalesEnfermeria.setPresion_arterial(Integer.parseInt(txt_presion_arterial.getText().toString()));
+                signosVitalesEnfermeria.setFrecuencia_cardiaca(Integer.parseInt(txt_pulso_por_minuto.getText().toString()));
+                signosVitalesEnfermeria.setTemperatura(Integer.parseInt(txt_temperatura.getText().toString()));
+
+                signosVitalesEnfermeria.save();
+
+                Toast.makeText(getContext(), "Motivo de Atencion guardado exitoso",Toast.LENGTH_SHORT).show();
+            }
+        });
         return view;
     }
 }
