e002346e6c367dceab3bba65a3e9538bd8aacad2
renato <rillesca@espol.edu.ec>
2018-01-23 19:47:47 -0500

    version 1.0 - correccion de variables signos vitales

--- app/src/main/java/ec/edu/espol/ingsoft/fichamedica/historia_medica/tabs/consulta_medica/SignosVitalesContentFragment.java
+++ app/src/main/java/ec/edu/espol/ingsoft/fichamedica/historia_medica/tabs/consulta_medica/SignosVitalesContentFragment.java
@@ -16,31 +16,30 @@ import ec.edu.espol.ingsoft.fichamedica.model.SignosVitales;
 public class SignosVitalesContentFragment extends Fragment {
 
     private static final String TAG = "SignosVitalesContentFragment";
-    EditText txtPresionArterial,
-            txtPulsoPorMinuto,
-            txtTemperatura,
-            txtmotivoConsulta;
-    Button btnGuardar;
+    EditText txt_presion_arterial,
+            txt_pulso_por_minuto,
+            txt_temperatura;
+    Button btn_guardar;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.signos_vitales_content_fragment,container,false);
 
-        txtPresionArterial = view.findViewById(R.id.txt_presion_arterial);
-        txtPulsoPorMinuto = view.findViewById(R.id.txt_pulso_por_minuto);
-        txtTemperatura = view.findViewById(R.id.txt_temperatura);
-        btnGuardar = view.findViewById(R.id.btn_guardar);
+        txt_presion_arterial = view.findViewById(R.id.txt_presion_arterial);
+        txt_pulso_por_minuto = view.findViewById(R.id.txt_pulso_por_minuto);
+        txt_temperatura = view.findViewById(R.id.txt_temperatura);
+        btn_guardar = view.findViewById(R.id.btn_guardar);
 
-        btnGuardar.setOnClickListener(new View.OnClickListener() {
+        btn_guardar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 SignosVitales signosVitalesPaciente = new SignosVitales();
 
-                signosVitalesPaciente.setPresion_arterial(Integer.parseInt(txtPresionArterial.getText().toString()));
-                signosVitalesPaciente.setFrecuencia_cardiaca(Integer.parseInt(txtPulsoPorMinuto.getText().toString()));
-                signosVitalesPaciente.setTemperatura(Integer.parseInt(txtTemperatura.getText().toString()));
-                
+                signosVitalesPaciente.setPresion_arterial(Integer.parseInt(txt_presion_arterial.getText().toString()));
+                signosVitalesPaciente.setFrecuencia_cardiaca(Integer.parseInt(txt_pulso_por_minuto.getText().toString()));
+                signosVitalesPaciente.setTemperatura(Integer.parseInt(txt_temperatura.getText().toString()));
+
                 signosVitalesPaciente.save();
 
                 Toast.makeText(getContext(),"Signos Vitales Guardados exitosamente",Toast.LENGTH_SHORT).show();
