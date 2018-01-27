fa3c39021dd9ca307b4912a740089689f22c98d0
jorge18link <jorge_18link@hotmail.com>
2018-01-27 02:13:00 -0500

    Version 1.0.5 - Ahora los botones para modificar cantidades en signos vitales sirven

--- app/src/main/java/ec/edu/espol/ingsoft/fichamedica/historia_medica/tabs/consulta_enfermeria/SignosVitalesEnfermeriaContentFragment.java
+++ app/src/main/java/ec/edu/espol/ingsoft/fichamedica/historia_medica/tabs/consulta_enfermeria/SignosVitalesEnfermeriaContentFragment.java
@@ -42,8 +42,8 @@ public class SignosVitalesEnfermeriaContentFragment extends Fragment {
         View view = inflater.inflate(R.layout.signos_vitales_enfermeria_content_fragment, container, false);
         idEmpleado = getArguments().getString("idEmpleado");
 
-        presionSistolica = (EditText) view.findViewById(R.id.txt_presion_distolica);
-        presionDistolica = (EditText) view.findViewById(R.id.txt_presion_sistolica);
+        presionSistolica = (EditText) view.findViewById(R.id.txt_presion_sistolica);
+        presionDistolica = (EditText) view.findViewById(R.id.txt_presion_distolica);
         pulsosPorMinuto = (EditText) view.findViewById(R.id.txt_pulso);
         temperatura = (EditText) view.findViewById(R.id.txt_temperatura);
 
@@ -56,6 +56,55 @@ public class SignosVitalesEnfermeriaContentFragment extends Fragment {
         masTemperatura = (Button) view.findViewById(R.id.btn_mas_temperatura);
         menosTemperatura = (Button) view.findViewById(R.id.btn_menos_temperatura);
         btn_guardar =     view.findViewById(R.id.btn_guardar);
+
+        masSis.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                buttonPress(view);
+            }
+        });
+        menosSis.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                buttonPress(view);
+            }
+        });
+        masDis.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                buttonPress(view);
+            }
+        });
+        menosDis.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                buttonPress(view);
+            }
+        });
+        masPulso.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                buttonPress(view);
+            }
+        });
+        menosPulso.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                buttonPress(view);
+            }
+        });
+        masTemperatura.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                buttonPress(view);
+            }
+        });
+        menosTemperatura.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                buttonPress(view);
+            }
+        });
 
 
 
@@ -72,25 +121,109 @@ public class SignosVitalesEnfermeriaContentFragment extends Fragment {
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd.MM.yyyy HH:mm");
         Date currentTime = Calendar.getInstance().getTime();
         String fechaActual = simpleDateFormat.format(currentTime);
-        int presionArterialSistolica = Integer.parseInt(presionSistolica.getText().toString());
-        int presionArterialDistolica = Integer.parseInt(presionDistolica.getText().toString());
-        int pulsos = Integer.parseInt(pulsosPorMinuto.getText().toString());
-        float tempert = Float.parseFloat(temperatura.getText().toString());
-
         SignosVitales signosVitales = new SignosVitales();
-        signosVitales.setIdEmpleado(idEmpleado);
-        signosVitales.setConsultaEnfermeria(true);
-        signosVitales.setFecha(fechaActual);
-        signosVitales.setPresionArterialSistolica(presionArterialSistolica);
-        signosVitales.setPresionArterialDistolica(presionArterialDistolica);
-        signosVitales.setFrecuenciaCardiaca(pulsos);
-        signosVitales.setTemperatura(tempert);
 
         try{
-            signosVitales.save();
-            Toast.makeText(getContext(),"Signos vitales guardados",Toast.LENGTH_LONG).show();
+            int presionArterialSistolica = Integer.parseInt(presionSistolica.getText().toString());
+            int presionArterialDistolica = Integer.parseInt(presionDistolica.getText().toString());
+            int pulsos = Integer.parseInt(pulsosPorMinuto.getText().toString());
+            float tempert = Float.parseFloat(temperatura.getText().toString());
+
+            signosVitales.setIdEmpleado(idEmpleado);
+            signosVitales.setConsultaEnfermeria(true);
+            signosVitales.setFecha(fechaActual);
+            signosVitales.setPresionArterialSistolica(presionArterialSistolica);
+            signosVitales.setPresionArterialDistolica(presionArterialDistolica);
+            signosVitales.setFrecuenciaCardiaca(pulsos);
+            signosVitales.setTemperatura(tempert);
+
+            try{
+                signosVitales.save();
+                Toast.makeText(getContext(),"Signos vitales guardados",Toast.LENGTH_LONG).show();
+            }catch (Exception e){
+                Toast.makeText(getContext(),e.getMessage(),Toast.LENGTH_LONG).show();
+            }
+
         }catch (Exception e){
-            Toast.makeText(getContext(),e.getMessage(),Toast.LENGTH_LONG).show();
+            Toast.makeText(getContext(),"Por favor ingrese todos los datos",Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public void buttonPress(View view) {
+        int valor;
+        switch (view.getId()) {
+            case R.id.btn_mas_sis:
+                if(presionSistolica.getText().toString().isEmpty()){
+                    presionSistolica.setText(String.valueOf(1));
+                }else{
+                    valor = Integer.parseInt(presionSistolica.getText().toString())+1;
+                    presionSistolica.setText(String.valueOf(valor));
+                }
+                break;
+            case R.id.btn_menos_sis:
+                if(presionSistolica.getText().toString().isEmpty()){
+                    presionSistolica.setText(String.valueOf(0));
+                }else{
+                    if(Integer.parseInt(presionSistolica.getText().toString())!=0){
+                        valor = Integer.parseInt(presionSistolica.getText().toString())-1;
+                        presionSistolica.setText(String.valueOf(valor));
+                    }
+                }
+                break;
+            case R.id.btn_mas_dis:
+                if(presionDistolica.getText().toString().isEmpty()){
+                    presionDistolica.setText(String.valueOf(1));
+                }else{
+                    valor = Integer.parseInt(presionDistolica.getText().toString())+1;
+                    presionDistolica.setText(String.valueOf(valor));
+                }
+                break;
+            case R.id.btn_menos_dis:
+                if(presionDistolica.getText().toString().isEmpty()){
+                    presionDistolica.setText(String.valueOf(0));
+                }else{
+                    if(Integer.parseInt(presionDistolica.getText().toString())!=0){
+                        valor = Integer.parseInt(presionDistolica.getText().toString())-1;
+                        presionDistolica.setText(String.valueOf(valor));
+                    }
+                }
+                break;
+            case R.id.btn_mas_pulso:
+                if(pulsosPorMinuto.getText().toString().isEmpty()){
+                    pulsosPorMinuto.setText(String.valueOf(1));
+                }else{
+                    valor = Integer.parseInt(pulsosPorMinuto.getText().toString())+1;
+                    pulsosPorMinuto.setText(String.valueOf(valor));
+                }
+                break;
+            case R.id.btn_menos_pulso:
+                if(pulsosPorMinuto.getText().toString().isEmpty()){
+                    pulsosPorMinuto.setText(String.valueOf(0));
+                }else{
+                    if(Integer.parseInt(pulsosPorMinuto.getText().toString())!=0){
+                        valor = Integer.parseInt(pulsosPorMinuto.getText().toString())-1;
+                        pulsosPorMinuto.setText(String.valueOf(valor));
+                    }
+                }
+                break;
+            case R.id.btn_mas_temperatura:
+                if(temperatura.getText().toString().isEmpty()){
+                    temperatura.setText(String.valueOf(1));
+                }else{
+                    valor = Integer.parseInt(temperatura.getText().toString())+1;
+                    temperatura.setText(String.valueOf(valor));
+                }
+                break;
+            case R.id.btn_menos_temperatura:
+                if(temperatura.getText().toString().isEmpty()){
+                    temperatura.setText(String.valueOf(0));
+                }else{
+                    if(Integer.parseInt(temperatura.getText().toString())!=0){
+                        valor = Integer.parseInt(temperatura.getText().toString())-1;
+                        temperatura.setText(String.valueOf(valor));
+                    }
+                }
+                break;
         }
     }
 }
